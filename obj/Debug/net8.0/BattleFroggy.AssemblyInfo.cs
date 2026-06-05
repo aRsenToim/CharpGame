ab11e3ebddc456740bab94610f3b367eb4c1ca74
Arsen Hajranov <arsen.hajranov123@gmail.com>
2026-06-05 05:50:30 +0500

    fix

--- obj/Debug/net8.0/BattleFroggy.AssemblyInfo.cs
+++ obj/Debug/net8.0/BattleFroggy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleFroggy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ecd6eef09710fc412ba9528954c4f063d5798de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a741da91486cc98ea393502d6425e0d749a676f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleFroggy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleFroggy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
